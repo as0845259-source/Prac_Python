87f2cc18883a7e209e1e3848561c4c24e663c082
as0845259-source <as0845259@gmail.com>
2026-08-28 18:19:59 +0500

    Add python file

---
+++ while-loop.py
@@ -0,0 +1,13 @@
+i = 1
+while i<=10 :
+    print(i,"I LOVE PYTHON")
+    i = i+1
+    
+print() # just for space 
+  
+#  In  reverse case
+
+a = 10
+while a>=1:
+    print(a , "I love python")
+    a = a-1
