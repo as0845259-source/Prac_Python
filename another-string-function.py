69803a5fd8a0a54f3d8091ef1ba706a7c47b23fd
as0845259-source <as0845259@gmail.com>
2026-08-29 15:25:03 +0500

    Add New Python File

---
+++ another-string-function.py
@@ -0,0 +1,21 @@
+f = "How are you"
+print(f.find('y',2))
+
+i = "i love python"
+print(i.index('v'))
+
+a = "python786"
+print(a.isalpha())
+
+d = "python786"
+print(d.isdigit())
+
+n = "python786"
+print(n.isalnum()) #when use special character the answer will be false
+
+
+
+
+    
+
+    
---
+++ string-function.py
@@ -0,0 +1,16 @@
+lower = "I LOVE PYTHON"
+l = lower.lower()
+print(l)
+
+upper = "i love pyhon"
+u = upper.upper()
+print(u)
+
+title = "i love python . i like python"
+t = title.title()
+print(t)
+
+capitalize = "i love python . wellcome to python"
+c = capitalize.capitalize()
+print(c)
+
