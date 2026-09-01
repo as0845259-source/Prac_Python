fd7232c94ca08e2206e44004a7f411f45ab995d4
as0845259-source <as0845259@gmail.com>
2026-09-01 10:45:33 +0500

    Add new python File

---
+++ list-function.py
@@ -0,0 +1,33 @@
+c = [10 , 20 , 30 , 40 , 50 , 10]
+o = c.count(10)   #count the value in list
+print(o)
+
+m = [10 , 20 , 30 , 40 , 50]
+a = max(m)  #find the maximum value in list
+print(a)
+
+b = min(m)   #find the mini value in list
+print(b)  
+
+
+
+m = ["Hello" , "World"]
+x = max(m)  #Find the string Z to A
+print(x)
+
+y = min(m)
+print(y) #Find the string  A to Z
+ 
+s = [ 30 , 20 , 50 , 10 , 40]
+s.sort()
+print(s)  # value in sort (sequence)
+  
+  
+r = [50 , 40 , 30 , 20 , 10 ]
+r.reverse() # reverse the value
+print(r)  
+
+
+Index = [10 , 20 , 30 , 40 , 50 , 60]
+d = Index.index(30)
+print(d)
