e39cf577b3bb7cf4cc0efd5b3716f608e6905351
as0845259-source <as0845259@gmail.com>
2026-09-03 15:41:51 +0500

    Update the file

--- project-2.py
+++ project-2.py
@@ -1,58 +1,58 @@
-    # Rock Paper Game
+# Rock Paper Game
 
-    import random
-    l = ['Rock', 'scissor', 'paper']
+import random
+l = ['Rock', 'scissor', 'paper']
 
-    while True:
-        ccount = 0
-        ucount = 0
-        uc = int(input('''
-    Game Start.....                   
-    1 Yes | Play
-    2 No  | Exit             
-                    '''))
-        
-        if uc == 1:
-            for a in range(1, 6):
-                userInput = int(input('''
-    1 Rock
-    2 Scissor
-    3 Paper                                                            
-                                    '''))
-                if userInput == 1:
-                    uchoice = 'Rock'
-                elif userInput == 2:
-                    uchoice = 'Scissor'
-                elif userInput == 3:
-                    uchoice = 'paper'
+while True:
+    ccount = 0
+    ucount = 0
+    uc = int(input('''
+Game Start.....                   
+1 Yes | Play
+2 No  | Exit             
+                   '''))
+    
+    if uc == 1:
+        for a in range(1, 6):
+            userInput = int(input('''
+1 Rock
+2 Scissor
+3 Paper                                                            
+                                  '''))
+            if userInput == 1:
+                uchoice = 'Rock'
+            elif userInput == 2:
+                uchoice = 'Scissor'
+            elif userInput == 3:
+                uchoice = 'paper'
 
-                Cchoice = random.choice(l)
+            Cchoice = random.choice(l)
 
-                if Cchoice == uchoice:
-                    ccount = ccount + 1
-                    ucount = ucount + 1
-                    winner = "Draw"
-                elif (uchoice == "paper" and Cchoice == "Rock") or (uchoice == "Scissor" and Cchoice == "paper") or (uchoice == "Rock" and Cchoice == "scissor"):
-                    ucount = ucount + 1
-                    winner = "User"
-                else:
-                    ccount = ccount + 1
-                    winner = "Computer"
-
-                print(f"Round {a}: User = {uchoice}, Computer = {Cchoice}, Winner = {winner}")
-
-            if ucount == ccount:
-                print("Final Game Draw...")
-                print("User Count:", ucount)
-                print("Computer Count:", ccount)
-            elif ucount > ccount:
-                print("Final You Win the Game...")
-                print("User Count:", ucount)
-                print("Computer Count:", ccount)
+            if Cchoice == uchoice:
+                ccount = ccount + 1
+                ucount = ucount + 1
+                winner = "Draw"
+            elif (uchoice == "paper" and Cchoice == "Rock") or (uchoice == "Scissor" and Cchoice == "paper") or (uchoice == "Rock" and Cchoice == "scissor"):
+                ucount = ucount + 1
+                winner = "User"
             else:
-                print("Final Computer win the Game...")
-                print("User Count:", ucount)
-                print("Computer Count:", ccount)
+                ccount = ccount + 1
+                winner = "Computer"
 
+            print(f"Round {a}: User = {uchoice}, Computer = {Cchoice}, Winner = {winner}")
+
+        if ucount == ccount:
+            print("Final Game Draw...")
+            print("User Count:", ucount)
+            print("Computer Count:", ccount)
+        elif ucount > ccount:
+            print("Final You Win the Game...")
+            print("User Count:", ucount)
+            print("Computer Count:", ccount)
         else:
-            break
+            print("Final Computer win the Game...")
+            print("User Count:", ucount)
+            print("Computer Count:", ccount)
+
+    else:
+        break
